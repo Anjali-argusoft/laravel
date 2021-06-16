49909780c39c8bb409089d69776efbedbfc0d6ba
Anjali-argusoft <agajjar#argusoft.com>
2021-06-16 17:23:30 +0530

    Insert Category using query builder

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -6,7 +6,9 @@ use App\Models\Category;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
-// use Illuminate\Support\Carbon;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
+
 class CategoryController extends Controller
 {
       public function getcategories(){
@@ -22,18 +24,26 @@ class CategoryController extends Controller
       'category_name.max' => 'Category name max limit exceed',
     ]);
 
-      // //Insert category using Eloquent ORM method
+      // Insert category using Eloquent ORM method
       //   Category::insert([
       //     'category_name' => $request->category_name,
       //     'user_id' => Auth::user()->id,
       //     'created_at' => Carbon::now()
       //   ]);
 
-      //Insert category using Eloquent ORM method second process
-      $category = new Category;
-      $category->category_name = $request->category_name;
-      $category->user_id = Auth::user()->id;
-      $category->save();
+      // Insert category using Eloquent ORM method second process
+      // $category = new Category;
+      // $category->category_name = $request->category_name;
+      // $category->user_id = Auth::user()->id;
+      // $category->save();
+
+      // Insert category using Query Builder method 
+        $date = array();
+        $data['category_name'] = $request->category_name;
+        $data['user_id'] = Auth::user()->id;
+        $data['created_at'] = Carbon::now();
+        DB::table('categories')->insert($data);
+
 
       return Redirect()->back()->with('success', 'Category inserted successfully');
   }
